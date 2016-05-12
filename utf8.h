4ca514f8addede61d892678f81e39d273dc55b71
Ben Woolley <ben@fw.benwoolley.org>
2016-05-11 17:53:54 -0700

    draft UTF-8 encoder/decoder

--- utf8.h
+++ utf8.h
@@ -2,7 +2,7 @@
 #define UTF8_H
 
 /*
- * UTF-8 Byte 1
+ * UTF-8 Decode Byte 1
  * Pass in `b` byte, and get `c` significant bits shifted from that byte, and get `l` length of the UTF-8 sequence, and get `s` status as a valid UTF-8 byte.
  */
 #define UTF8_B1(b, c, l, s) { \
@@ -26,15 +26,15 @@
 }
 
 /*
- * UTF-8 Byte Next (2+)
+ * UTF-8 Decode Byte Next (2+)
  * Pass in `b` byte, and get `c` significant bits shifted from that byte, and get `s` status as a valid UTF-8 byte.
  */
 #define UTF8_BN(b, c, s) { \
-	if ((b & 0xc0) == 0x80) { \
-		c = (c << 6) | (b & 0x3f); \
-		s = 1; \
+	if (((b) & 0xc0) == 0x80) { \
+		(c) = ((c) << 6) | ((b) & 0x3f); \
+		(s) = 1; \
 	} else { \
-		s = 0; \
+		(s) = 0; \
 	} \
 }
 
@@ -63,7 +63,7 @@
 }
 
 /*
- * UTF-8 Character
+ * UTF-8 Decode Character
  * Pass in `ba` byte array and `bl` byte array length, and get `c` character and `cl` character length in bytes.
  * cl >  0:  `cl` length of a valid UTF-8 sequence.
  * cl == 0:  `cl` is too long for `bl`.
@@ -98,7 +98,7 @@
 }
 
 /*
- * UTF-8 Decode
+ * UTF-8 Decode String
  * Pass in `bc` byte cursor, `bs` byte stop, `cc` character cursor, and `cs` character stop.
  * The cursors will be updated as UTF-8 is parsed and characters are emitted, until:
  *  1. a cursor reaches a stop address.
@@ -126,42 +126,76 @@
  * UTF-8 Character Byte Length
  * Pass in `c` character, and get `cl` byte length.
  */
-#define UTF8_LEN(c, cl) { \
-	if (c < 0) { \
-		cl = 0; \
-	} else if (c < 0x80) { \
-		cl = 1; \
-	} else if (c < 0x800) { \
-		cl = 2; \
-	} else if (c < 0x10000) { \
-		cl = 3; \
-	} else if (c < 0x200000) { \
-		cl = 4; \
-	} else if (c < 0x4000000) { \
-		cl = 5; \
-	} else if (c < 0x80000000) { \
-		cl = 6; \
+#define UTF8_LEN(c, l) { \
+	if ((c) < 0) { \
+		(l) = 0; \
+	} else if ((c) < 0x80) { \
+		(l) = 1; \
+	} else if ((c) < 0x800) { \
+		(l) = 2; \
+	} else if ((c) < 0x10000) { \
+		(l) = 3; \
+	} else if ((c) < 0x200000) { \
+		(l) = 4; \
+	} else if ((c) < 0x4000000) { \
+		(l) = 5; \
+	} else if ((c) < 0x80000000) { \
+		(l) = 6; \
 	} else { \
-		cl = 0 ; \
+		(l) = 0 ; \
 	} \
 }
 
+/*
+ * UTF-8 Encode Character Byte 1
+ * Pass in `l` byte length from UTF8_LEN(), and get bits from `c` shifted into `b` based on `l`.
+ */
+#define UTF8_C1(c, b, l) { \
+	(b) = ((0xFF << (8 - l)) & 0xFF) | ((c) & ((1 << (7 - l)) - 1)); \
+	(c) >>= 7 - l; \
+}
+
+/*
+ * UTF-8 Encode Character Byte Next (2+)
+ * Get 6 bits from `c` shifted into `b` with the continuation high-bits set.
+ */
 #define UTF8_CN(c, b) { \
 	(b) = 0xc0 | (c) & 0x3f; \
 	(c) >>= 6; \
 }
 
+/*
+ * UTF8-8 Encode String
+ * Pass in `cc` character cursor, `cs` character stop, `bc` byte cursor, and `bs` byte stop.
+ * The cursors will be updated as UTF-8 is parsed and characters are emitted, until:
+ *  1. a cursor reaches a stop address.
+ *  2. a complete sequence would run past the byte stop address.
+ */
 #define UTF8_ENCODE(cc, cs, bc, bs) { \
 	int __utf8_cl; \
+	wchar_t c; \
 	while ((cc) < (cs) && (bc) < (bs)) { \
 		UTF8_LEN(*(cc), __utf8_cl); \
 		if (__utf8_cl == 1) { /* ASCII */ \
 			*((bc)++) = *((cc)++); \
 			continue; \
 		} else if (__utf8_cl > 1) { \
-			if ((bc) + __utf8_cl < (bs)) { \
-				*(bc) = ((0xFF << (8 - __utf8_cl)) & 0xFF) | ((*(cc)) & ((1 << (7 - __utf8_cl)) - 1)); \
-				*(cc) >>= 7 - __utf8_cl; \
+			if ((bc) + __utf8_cl < (bs)) { /* character fits */ \
+				c = *(cc); \
+				switch (__utf8_cl) { \
+					case 6: \
+						UTF8_CN(c, (bc)[5]); \
+					case 5: \
+						UTF8_CN(c, (bc)[4]); \
+					case 4: \
+						UTF8_CN(c, (bc)[3]); \
+					case 3: \
+						UTF8_CN(c, (bc)[2]); \
+					case 2: \
+						UTF8_CN(c, (bc)[1]); \
+						UTF8_C1(c, (bc)[0], __utf8_cl); \
+						break; \
+				} \
 			} else { \
 				break; /* blocking on byte length */ \
 			} \
