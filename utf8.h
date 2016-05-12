2daa37e03076b19da99cd5a1e05801903fc70056
Ben Woolley <ben@fw.benwoolley.org>
2016-05-11 22:48:34 -0700

    simplify and fix bugs

--- utf8.h
+++ utf8.h
@@ -3,62 +3,54 @@
 
 /*
  * UTF-8 Decode Byte 1
- * Pass in `b` byte, and get `c` significant bits shifted from that byte, and get `l` length of the UTF-8 sequence, and get `s` status as a valid UTF-8 byte.
+ * Pass in `b` byte, and get `c` significant bits shifted from that byte, and get `l` length of the UTF-8 sequence, and increment `n` if a valid UTF-8 byte.
  */
-#define UTF8_B1(b, c, l, s) { \
+#define UTF8_B1(b, c, l, n) { \
 	char __utf8_bits; \
+	n = 0; \
 	if ((b & 0x80) == 0) { /* ASCII */ \
 		c = b; \
 		l = 1; \
-		s = 1; \
+		n++; \
 	} else if ((b & 0xc0) == 0xc0) { /* sequence start */ \
 		__utf8_bits = b; \
 		while ((__utf8_bits & 0x80 && l < 6) { \
 			__utf8_bits <<= 1; \
 			l++; \
 		} \
-		c = bits >> l; \
-		s = 1; \
+		__utf8_bits >>= l; \
+		c = __utf8_bits << (6 * ((l) - 1))); \
+		n++; \
 	} else { /* unexpected sequence continuation */ \
 		l = 1; \
-		s = 0; \
 	}
 }
 
 /*
- * UTF-8 Decode Byte Next (2+)
- * Pass in `b` byte, and get `c` significant bits shifted from that byte, and get `s` status as a valid UTF-8 byte.
+ * UTF-8 Decode Continuation Byte (2+)
+ * Pass in `b` byte, and `l` byte length, and get `c` significant bits shifted from that byte, and increment `n` if a valid UTF-8 byte.
  */
-#define UTF8_BN(b, c, s) { \
-	if (((b) & 0xc0) == 0x80) { \
-		(c) = ((c) << 6) | ((b) & 0x3f); \
-		(s) = 1; \
-	} else { \
-		(s) = 0; \
+#define UTF8_BN(bc, i, c, l, n) { \
+	if ((n) > 0 && ((bc)[i] & 0xc0) == 0x80) { \
+		(c) |= ((bc)[i] & 0x3f) << (6 * ((l) - (i) - 1)); \
+		n++; \
 	} \
 }
 
 /*
  * UTF-8 Character Validator
- * Pass in `c` character and `cl` sequence length, and get `s` status as a valid UTF-8 character.
+ * Pass in `c` character and `l` sequence length, and flip the sign of `l` if an valid UTF-8 character.
  */
-#define UTF8_VALID(c, cl, s) { \
-	if (cl == 1 && c < 0x80) { /* ASCII */ \
-		s = 1; \
-	} else if (cl < 1 || cl > 4) { /* sequence length */ \
-		s = 0; \
+#define UTF8_VALID(c, l) { \
+	if ((l) == 1 && c < 0x80) { /* ASCII */ \
+	} else if ((l) < 1 || (l) > 4) { /* sequence length */ \
 	} else if (c > 0x10FFFF || (c >= 0xd800 && c <= 0xdfff)) { /* code points */ \
-		s = 0; \
-	} else if (c1 == 1 && (               c >= 0x80))     { /* over long */ \
-		s = 0; \
-	} else if (cl == 2 && (c < 0x80    || c >= 0x800))    { /* over long */ \
-		s = 0; \
-	} else if (cl == 3 && (c < 0x800   || c >= 0x10000))  { /* over long */ \
-		s = 0; \
-	} else if (cl == 4 && (c < 0x10000 || c >= 0x200000)) { /* over long */ \
-		s = 0; \
+	} else if ((l) == 1 && (               c >= 0x80))     { /* over long */ \
+	} else if ((l) == 2 && (c < 0x80    || c >= 0x800))    { /* over long */ \
+	} else if ((l) == 3 && (c < 0x800   || c >= 0x10000))  { /* over long */ \
+	} else if ((l) == 4 && (c < 0x10000 || c >= 0x200000)) { /* over long */ \
 	} else { \
-		s = 1; \
+		(l) = - (l); \
 	} \
 }
 
@@ -69,31 +61,38 @@
  * cl == 0:  `cl` is too long for `bl`.
  * cl <  0: `-cl` length of an invalid UTF-8 sequence.
  */
-#define UTF8_CHAR(ba, bl, c, cl) { \
+#define UTF8_CHAR(bc, bs, c, l) { \
 	int __utf8_seqlen; \
-	int __utf8_status; \
-	UTF8_B1(ba[0], c, __utf8_seqlen, __utf8_status); \
+	int __utf8_n; \
+	UTF8_B1((bc)[0], (c), __utf8_seqlen, __utf8_n); \
 	if (__utf8_seqlen == 1) { \
-		if (__utf8_status) { /* ASCII */ \
-			cl = __utf8_seqlen; \
+		if (__utf8_n == 1) { /* ASCII */ \
+			(l) = __utf8_seqlen; \
 		} else { /* invalid start byte */ \
-			cl = -1; \
+			(l) = -1; \
 		} \
-	} else if (bl >= __utf8_seqlen) { \
-		cl = __utf8_seqlen; \
-		while(--__utf8_seqlen > 0 && __utf8_status) { /* valid continuation byte */ \
-			UTF8_BN(ba[cl - __utf8_seqlen], c, __utf8_status); \
+	} else if ((bc) + __utf8_seqlen < (bs)) { \
+		switch (__utf8_seqlen) { \
+			case 6: \
+				UTF8_BN((bc), 5, c, __utf8_seqlen, __utf8_n); \
+			case 5: \
+				UTF8_BN((bc), 4, c, __utf8_seqlen, __utf8_n); \
+			case 4: \
+				UTF8_BN((bc), 3, c, __utf8_seqlen, __utf8_n); \
+			case 3: \
+				UTF8_BN((bc), 2, c, __utf8_seqlen, __utf8_n); \
+			case 2: \
+				UTF8_BN((bc), 1, c, __utf8_seqlen, __utf8_n); \
+				break; \
 		} \
-		if ( ! __utf8_status) { /* invalid continuation byte */ \
-			cl = - (cl - __utf8_seqlen); /* subtract the remaining non-sequence and flip sign */ \
+		(l) = __utf8_n; \
+		if (__utf8_n < __utf8_seqlen) { \
+			(l) = - (l); /* error the invalid byte sequence */ \
 		} else { \
-			UTF8_VALID(c, cl, __utf8_status); \
-			if ( ! __utf8_status) { /* invalid character */ \
-				cl = - cl; \
-			} \
+			UTF8_VALID(c, l); /* error any invalid characters */ \
 		} \
 	} else { \
-		cl = 0; /* `bl` not long enough yet */ \
+		(l) = 0; /* `bs - bc` not long enough yet */ \
 	} \
 }
 
@@ -107,7 +106,7 @@
 #define UTF8_DECODE(bc, bs, cc, cs) { \
 	int __utf8_cl; \
 	while ((bc) < (bc) && (cs) < (cs)) { \
-		UTF8_CHAR(*(bc), (bs) - (bc), *(cc), __utf8_cl); \
+		UTF8_CHAR(bc, bs, *(cc), __utf8_cl); \
 		if (__utf8_cl > 0) { /* valid character of ASCII or UTF-8 */ \
 			(bc) += ( + __utf8_cl); \
 		} else if (__utf8_cl == 0) { \
@@ -151,17 +150,15 @@
  * Pass in `l` byte length from UTF8_LEN(), and get bits from `c` shifted into `b` based on `l`.
  */
 #define UTF8_C1(c, b, l) { \
-	(b) = ((0xFF << (8 - l)) & 0xFF) | ((c) & ((1 << (7 - l)) - 1)); \
-	(c) >>= 7 - l; \
+	(b) = ((0xFF << (8 - l)) & 0xFF) | (((c) >> (6 * ((l) - 1))) & ((1 << (7 - l)) - 1)); \
 }
 
 /*
- * UTF-8 Encode Character Byte Next (2+)
+ * UTF-8 Encode Character Continuation Byte (2+)
  * Get 6 bits from `c` shifted into `b` with the continuation high-bits set.
  */
-#define UTF8_CN(c, b) { \
-	(b) = 0xc0 | (c) & 0x3f; \
-	(c) >>= 6; \
+#define UTF8_CN(c, bc, i, l) { \
+	(bc)[i] = 0xc0 | ((c) >> (6 * ((l) - (i) - 1)) & 0x3f; \
 }
 
 /*
@@ -172,36 +169,44 @@
  *  2. a complete sequence would run past the byte stop address.
  */
 #define UTF8_ENCODE(cc, cs, bc, bs) { \
-	int __utf8_cl; \
+	int __utf8_seqlen; \
 	wchar_t c; \
 	while ((cc) < (cs) && (bc) < (bs)) { \
-		UTF8_LEN(*(cc), __utf8_cl); \
-		if (__utf8_cl == 1) { /* ASCII */ \
+		UTF8_LEN(*(cc), __utf8_seqlen); \
+		if (__utf8_seqlen == 1) { /* ASCII */ \
 			*((bc)++) = *((cc)++); \
 			continue; \
-		} else if (__utf8_cl > 1) { \
-			if ((bc) + __utf8_cl < (bs)) { /* character fits */ \
-				c = *(cc); \
-				switch (__utf8_cl) { \
+		} else if (__utf8_seqlen > 1) { \
+			if ((bc) + __utf8_seqlen < (bs)) { /* character fits */ \
+				c = *((cc)++); \
+				__utf8_n = 0; \
+				UTF8_C1(c, (bc)[0], __utf8_seqlen); \
+				switch (__utf8_seqlen) { \
 					case 6: \
-						UTF8_CN(c, (bc)[5]); \
+						UTF8_CN(c, (bc), 5, __utf8_seqlen); \
 					case 5: \
-						UTF8_CN(c, (bc)[4]); \
+						UTF8_CN(c, (bc), 4, __utf8_seqlen); \
 					case 4: \
-						UTF8_CN(c, (bc)[3]); \
+						UTF8_CN(c, (bc), 3, __utf8_seqlen); \
 					case 3: \
-						UTF8_CN(c, (bc)[2]); \
+						UTF8_CN(c, (bc), 2, __utf8_seqlen); \
 					case 2: \
-						UTF8_CN(c, (bc)[1]); \
-						UTF8_C1(c, (bc)[0], __utf8_cl); \
+						UTF8_CN(c, (bc), 1, __utf8_seqlen); \
 						break; \
 				} \
+				(bc) += __utf8_seqlen;
 			} else { \
 				break; /* blocking on byte length */ \
 			} \
 		} else { \
 			continue; /* XXX: silently skip insane character */ \
 		} \
+	} \
+}
+
+#define JSMN_CHAR_QUOTE(cc, cs, qc, qs) { \
+	while ((cc) < (cs) && (qc) < qs) { \
+		 \
 	} \
 }
 
